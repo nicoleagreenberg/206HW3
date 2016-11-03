c418651e5c56be136fbe9e17ec0c2cfed2439cfb
Nicole Greenberg <nicoleag@umich.edu>
2016-11-02 20:55:18 -0400

    complete madlib program with comments

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -11,9 +11,7 @@
 # 1) Print the new text
 
 print("START*******")
-# code developed by Jackie Cohen; revised by Paul Resnick
-# further revised by Colleen van Lent for Python3
-import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+import nltk 
 import random
 from nltk.book import text2 
 from nltk import word_tokenize,sent_tokenize
@@ -23,11 +21,8 @@ debug = False #True
 # get file from user to make mad lib out of
 if debug:
 	print ("Getting information from file madlib_test.txt...\n")
-# fname = "text2" # need a file with this name in directory
 
-# f = open(fname, 'r')
-# para = f.read())
-para = nltk.word_tokenize(text2)
+tokens = text2[0:150]
 print("TOKENS")
 print(tokens)
 tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
@@ -36,10 +31,10 @@ print(tagged_tokens)
 if debug:
 	print ("First few tagged tokens are:")
 	for tup in tagged_tokens[:5]:
-		print (tup)
+		print(tup)
 
-tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
-substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective", "ADV": "an adverb"} #added adverb
+substitution_probabilities = {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1, "ADV": .1} #set frequencies
 
 def spaced(word):
 	if word in [",", ".", "?", "!", ":"]:
@@ -49,7 +44,6 @@ def spaced(word):
 
 final_words = []
 
-
 for (word, tag) in tagged_tokens:
 	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
 		final_words.append(spaced(word))
@@ -58,6 +52,5 @@ for (word, tag) in tagged_tokens:
 		final_words.append(spaced(new_word))
 
 print ("".join(final_words))
-
 
 print("\n\nEND*******")
